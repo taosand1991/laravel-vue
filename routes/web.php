ad2c36aee00405b4813b65ac5f4c328e2981ad79
TAOSAND <taofeekdboy@yahoo.com>
2021-01-01 22:45:05 +0400

    laravel vue

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::view('/', 'welcome');
-// Route::get('/{vue_capture?}', function () {
-//     return view('layouts.app');
-// })->where('vue_capture', '[\/\w\.-]*');
+// Route::view('/', 'welcome');
+Route::get('/{vue_capture?}', function () {
+    return view('layouts.app');
+})->where('vue_capture', '[\/\w\.-]*');
 
-// Route::view('/reset', 'welcome')->name('password.reset');
+Route::view('/reset', 'welcome')->name('password.reset');
