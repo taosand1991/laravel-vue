b7d26313be397196c89437c1db4bbebba790ad5f
TAOSAND <taofeekdboy@yahoo.com>
2021-01-02 11:34:28 +0400

    laravel vue

--- database/migrations/2020_12_28_182447_create_posts_table.php
+++ database/migrations/2020_12_28_182447_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('body');
+            $table->text('body');
             $table->foreignId('user_id');
             $table->json('likes')->nullable()->change();
             $table->timestamps();
